0e1040f40550e068dfd995fab6b074a539d56efb
rikza30 <3103120198@student.smktelkom-pwt.sch.id>
2022-10-19 15:27:21 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 
 
 /*
@@ -20,4 +21,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('me', [AuthController::class, 'me']);
+Route::get('me', [AuthController::class, 'me']
+);
+
+Route::resource('book', BookController::class)->except(
+    ['create','edit']
+);
